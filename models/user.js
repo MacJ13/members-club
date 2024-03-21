37c9b6f2aef27ec3f1123ee62f1e647e59b815a4
MacJ13 <jacdim02@gmail.com>
2024-03-21 20:32:26 +0100

    Create user model Schema

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  fullname: { type: String, required: true, minLength: 8 },
+  nickname: { type: String, required: true, minLength: 3 },
+  password: { type: String, required: true },
+  membership_status: { type: Boolean, default: false },
+  messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
+});
+
+userSchema.virtual("url").get(function () {
+  return `/user/${this._id}`;
+});
+
+module.exports = mongoose.model("user", userSchema);
