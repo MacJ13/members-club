a53d2a0d2847a8a2a1bd3e6748d8d717b78364d7
MacJ13 <jacdim02@gmail.com>
2024-03-26 22:43:14 +0100

    Update user model

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  fullname: { type: String, required: true, minLength: 8 },
+  fullname: { type: String, required: true, minLength: 6 },
   nickname: { type: String, required: true, minLength: 3 },
   password: { type: String, required: true },
   membership_status: { type: Boolean, default: false },
