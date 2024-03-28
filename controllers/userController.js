404cfb5dcae238626643e224742e6c42c13cf984
MacJ13 <jacdim02@gmail.com>
2024-03-28 21:47:33 +0100

    Create user controller

---
+++ controllers/userController.js
@@ -0,0 +1,17 @@
+const User = require("../models/user");
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const passport = require("passport");
+
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
+
+// display index home
+exports.index_get = (req, res, next) => {
+  //   console.log("user in index ", req.user);
+
+  const logged = Boolean(req.user);
+
+  res.render("index", { title: "Home page", user: req.user, logged: logged });
+};
