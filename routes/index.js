e2b66f05f966de944106429d143fd365d78e6eca
MacJ13 <jacdim02@gmail.com>
2024-03-28 21:48:13 +0100

    Create router index get

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+const user_controller = require("../controllers/userController");
 
 // GET signup page
-router.get("/sign-up", function (req, res, next) {
-  res.render("signup", { title: "Sign Up", user: undefined });
-});
+// router.get("/sign-up", function (req, res, next) {
+//   if (req.user) {
+//     res.redirect("/");
+//     return;
+//   }
+//   res.render("signup", {
+//     title: "Sign Up",
+//   });
+// });
 
 // GET login page
-router.get("/login", function (req, res, next) {
-  res.render("login", { title: "Log In", user: undefined });
-});
+// router.get("/login", function (req, res, next) {
+//   if (req.user) {
+//     res.redirect("/");
+//     return;
+//   }
+
+//   // console.log(req.session.messages);
+//   res.render("login", { title: "Log In", messages: req.session.messages });
+// });
+
+/* GET home page. */
+router.get("/", user_controller.index_get);
 
 module.exports = router;
