19ba45ce292c97fb022d85a53d94a5fd8f5bee05
MacJ13 <jacdim02@gmail.com>
2024-04-02 20:28:06 +0200

    Create get and post user join routes

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,10 @@ const message_controller = require("../controllers/messageController");
 const user_contorller = require("../controllers/userController");
 require("dotenv").config();
 
+router.get("/:id/join", user_contorller.user_memberclub_get);
+
+router.post("/:id/join", user_contorller.user_memberclub_post);
+
 /* GET users listing. */
 router.get("/:id", user_contorller.user_index);
 
