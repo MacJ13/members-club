b9a5032d8e4295cc136f831a78f73b68fd79452f
MacJ13 <jacdim02@gmail.com>
2024-04-02 20:16:06 +0200

    update Message model with add url

--- models/message.js
+++ models/message.js
@@ -4,9 +4,13 @@ const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
   title: { type: String, required: true, minLength: 3 },
-  timeStamp: { type: Date, default: new Date() },
+  timeStamp: { type: Date, default: Date.now },
   text: { type: String, required: true, minLength: 8 },
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+messageSchema.virtual("url").get(function () {
+  return `/message/${this._id}`;
 });
 
 module.exports = mongoose.model("message", messageSchema);
